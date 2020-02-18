c7df2837b02545a61e15a920d9bcd1f0c2256453
Dmitriy Shelestovskiy <dmitriy.shelestovski@neurohive.io>
2020-02-18 19:04:02 +0300

    cont

---
+++ generic_search.py
@@ -0,0 +1,28 @@
+
+class Stack(Generic[T]):
+    def __init__(self) -> None:
+        self._container: List[T] = []
+
+    @property
+    def empty(self) -> bool:
+        return not self._container
+
+    def push(self, item: T) -> None:
+        self._container.append(item)
+
+    def pop(self, item: T) -> None:
+        return self._container.pop()
+
+    def __repr__(self) -> str:
+        return repr(self._container)
+
+
+class Node(Generic[T]):
+    def __init__(self, state: T, parent: Optional[Node], cost: float = 0.0, heuristic: float = 0.0) -> None:
+        self.state: T = state
+        self.parent: Optional[Node] = parent
+        self.cost: float = cost
+        self.heuristic: float = heuristic
+
+    def __lt__(self, other: Node) -> bool:
+        return (self.cost + self.heuristic) < (other.cost + other.heuristic)
